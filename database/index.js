d1a32cef1ea1d4316d98437b0590a9cbb01158d7
acea68 <alancea01@gmail.com>
2022-05-04 11:27:45 -0500

    restarts setup without keys

--- database/index.js
+++ database/index.js
@@ -1,9 +1,5 @@
 import mongoose from 'mongoose';
-// const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher', { useNewUrlParser: true });
-// require('dotenv').config();
-// const client = new MongoClient(process.env.URI);
-// mongoose.connect(process.env.URI, { useNewUrlParser: true });
 
 let repoSchema = mongoose.Schema({
   id: {
@@ -20,24 +16,14 @@ let repoSchema = mongoose.Schema({
   html_url: String
 }); // can reduce amount of properties, but nice to have several options to filter search results by
 
-
-// This creates our model from the above schema, using mongoose's model method
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (repo) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to the MongoDB
+let saveEntry = (repo) => {
 
-  // check if the repo exists by trying to find it within the repo
-  // only is the repo doesn't exists, add it to the DB
   // console.log('repo: ', repo);
   Repo.update(
-    {
-      id: repo.id
-    },
-    {
-      $setOnInsert: repo
-    },
+    { id: repo.id },
+    { $setOnInsert: repo },
     { upsert: true }
   )
     .then((res) => {
@@ -46,9 +32,8 @@ let save = (repo) => {
     .catch((error) => {
       console.warn(error);
     })
-}
+};
 
 
-
-module.exports.save = save;
+module.exports.saveEntry = saveEntry;
 module.exports.Repo = Repo;
